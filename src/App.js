137a22405ee770d7528612ee750b3cf99972c111
Przemek <przemek.szumczyk@gmail.com>
2023-04-15 18:30:54 +0200

    Create the menubar. Create jest the visual side, without any functionalities.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+
   return (
     <div className="App">
       <div className="content">
         <div className="title">Axbit shuffle challenge</div>
+        <div className="main-container">
+          <div className="menu-bar">
+            <div className="buttons-section">
+              <button>Shuffle</button>
+              <button>Sort</button>
+            </div>
+            <div className="input-section">
+              <form>
+                <label>
+                  <input type="text" placeholder="type..." />
+                </label>
+              </form>
+            </div>
+          </div>
+          <div className="display-section"></div>
+        </div>
       </div>
     </div>
   );
